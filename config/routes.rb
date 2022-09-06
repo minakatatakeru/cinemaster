36d122e4455ba6c1fa79b472d9b750622d1ccd65
minakatatakeru <ec2-user@ip-172-31-43-59.ec2.internal>
2022-09-06 08:46:03 +0000

    cinemaモデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  # 顧客用
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+  devise_scope :admin do
+    get '/admin/sign_out' => 'admin/sessions#destroy'
+  end
+  
+  scope module: :public do
+   root to: "homes#top"
+  end
+  
+  namespace :admin do
+   root to: "homes#top"
+  end 
+  
 end
