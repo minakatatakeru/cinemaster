629a78f6348940d17be1bc6a0b19305f099749be
minakatatakeru <106653078+minakatatakeru@users.noreply.github.com>
2022-09-13 07:49:41 +0000

    [Fix]TMDbAPI

--- app/controllers/public/movie_controller.rb
+++ app/controllers/public/movie_controller.rb
@@ -1,4 +1,10 @@
 class Public::MovieController < ApplicationController
+
+  require 'themoviedb-api'
+  Tmdb::Api.key("0c76fa476747c678b8e71329680c5b5e")
+  # 映画情報の表示の際の言語設定を日本語にする
+  Tmdb::Api.language("ja")
+
   def index
   end
 
