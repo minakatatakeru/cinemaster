56d0094f5b2d71f0562681b529903e7905f99059
minakatatakeru <106653078+minakatatakeru@users.noreply.github.com>
2022-09-13 06:15:07 +0000

    [Feateure]TMDbAPI

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,2 +1,8 @@
 class Public::HomesController < ApplicationController
+
+  require 'themoviedb-api'
+  Tmdb::Api.key("0c76fa476747c678b8e71329680c5b5e")
+  # 映画情報の表示の際の言語設定を日本語にする
+  Tmdb::Api.language("ja")
+
 end
