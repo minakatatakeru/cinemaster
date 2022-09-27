48f66c98dfe1bdf9643a202b63cb3bc45197e92d
minakatatakeru <106653078+minakatatakeru@users.noreply.github.com>
2022-09-27 10:20:54 +0000

    [Fix]admin

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CommentsController < ApplicationController
    before_action :authenticate_customer!
    before_action :if_not_admin
- 　 before_action :set_comment, only: [:index, :new, :create, :show, :edit, :destroy]
+   before_action :set_comment, only: [:index, :new, :create, :show, :edit, :destroy]
 
 def index
    @comments = Comment.all
@@ -14,12 +14,12 @@ def destroy
 end
 
  private
-　def if_not_admin
-　   redirect_to root_path unless current_customer.admin?
-　end
+  def if_not_admin
+    redirect_to root_path unless current_customer.admin?
+  end
 
   def set_comment
-　　 @comment = Comment.find(params[:id])
+     @comment = Comment.find(params[:id])
   end
 
   def comment_params
