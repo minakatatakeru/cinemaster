d312ca82a94de052c2051a3b0069f84bdb3fe392
minakatatakeru <106653078+minakatatakeru@users.noreply.github.com>
2022-10-29 14:18:51 +0000

    [Fix]customers_controller

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,16 +1,3 @@
-class Public::CustomersController < ApplicationController
-
-  def index
-    @customers = Customer.all
-  end
-
-  def show
-    @customer = Customer.find(params[:id])
-  end
-
-  def edit
-    @customer = Customer.find(params[:id])
-  end
 
   def update
     @customer = Customer.find(params[:id])
