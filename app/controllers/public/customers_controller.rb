62f1604f1bf579004fac9c566ca04a5c3de6a003
minakatatakeru <106653078+minakatatakeru@users.noreply.github.com>
2022-11-20 14:44:25 +0000

    [Fix]customers_controller

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,28 +1,4 @@
-class Public::CustomersController < ApplicationController
 
-  # require 'themoviedb-api'
-  # Tmdb::Api.key("d6599ce988e5da53192a7a5539607824")
-  # # 映画情報の表示の際の言語設定を日本語にする
-  # Tmdb::Api.language("ja")
-
-  def index
-    @customers = Customer.all
-  end
-
-  def show
-    @customer = Customer.find(params[:id])
-  end
-
-  def edit
-    @customer = Customer.find(params[:id])
-  end
-
-  def update
-    @customer = Customer.find(params[:id])
-    if @customer.update(customer_params)
-       redirect_to customer_path, notice: "You have updated user successfully."
-    else
-       render :edit
     end
   end
 
