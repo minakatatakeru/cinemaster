f341257b444bdd981afe6485a42ed128bacedc99
minakatatakeru <106653078+minakatatakeru@users.noreply.github.com>
2022-11-25 14:45:04 +0000

    [Fix]customers_controller

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,25 @@
+class Public::CustomersController < ApplicationController
 
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+       redirect_to customer_path, notice: "You have updated user successfully."
+    else
+       render :edit
+    end
+  end
 
   def destroy
     # @comment = Comment.find(params[:id])
