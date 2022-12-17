1c2883fbc70c4f17c8e2391368332a93faf7a906
minakatatakeru <106653078+minakatatakeru@users.noreply.github.com>
2022-12-17 12:18:10 +0000

    [Fix]customers_controller

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,10 @@
 class Public::CustomersController < ApplicationController
 
+  # require 'themoviedb-api'
+  # Tmdb::Api.key("d6599ce988e5da53192a7a5539607824")
+  # # 映画情報の表示の際の言語設定を日本語にする
+  # Tmdb::Api.language("ja")
+
   def index
     @customers = Customer.all
   end
